6ed3cb1bf77ce1252772cd159dceca9d18c85ef0
Jonathan castillo <jonathan.castillo.itksquare.edu.mx>
2022-12-10 12:42:49 -0600

    Upload files

---
+++ index.js
@@ -0,0 +1,26 @@
+//Function that, when the user scrolls down the page, displays the nav bar.
+window.onscroll = function () {
+    scroll()
+};
+
+/*
+In this function the top position of the body of the page is checked, if it is less than 20px
+the navigation bar keeps its top position in 0px, otherwise it is displayed in 100px.    
+*/
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.querySelector(".navbar").style.top = "0";
+    }
+    else {
+        document.querySelector(".navbar").style.top = "-100px";
+    }
+}
+
+// let sound = document.querySelector(".sound");
+// sound.volume = 0.066
+// document.body.addEventListener('mouseover', () => {
+//     sound.muted = false;
+//     sound.play();
+// });
+
+
